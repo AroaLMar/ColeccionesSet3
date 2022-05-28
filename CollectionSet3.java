6be16f09dfcc642db4d579880a8827de6ef33655
AroaLMar <aroaliquetem@gmail.com>
2022-05-29 01:08:22 +0200

    TreeSet

---
+++ CollectionSet3.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+import java.util.TreeSet;
+
+public class CollectionSet3 {
+
+    public static void main(String[] args) {
+
+            // Declaracion
+
+            TreeSet<String> paises = new TreeSet<String>();
+
+            // Añadir Elementos
+            paises.add("España");
+            paises.add("Francia");
+            paises.add("Alemania");
+            paises.add("Colombia");
+            paises.add("Italia");
+            paises.add("Grecia");
+
+            //Ver colecciones
+
+            System.out.println(paises);
+
+    }
+}
